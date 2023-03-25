82aa1b9a4c6a861e56fdc374c9dbb2a20575020a
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2023-03-26 03:52:48 +0600

    appointment booking related work

--- index.js
+++ index.js
@@ -28,14 +28,43 @@ async function run() {
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
 
         app.get('/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
+            const bookingQuery = { appointmentData: date }
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+
+                const bookedSlots = optionBooked.map(book => book.slot)
+
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+
+                option.slots = remainingSlots;
+
+                // console.log(date, option.name, bookedSlots);
+
+            })
+
             res.send(options);
         });
 
         app.post('/bookings', async (req, res) => {
-            const booking = req.body
+            const booking = req.body;
             console.log(booking);
+            const query = {
+                appointmentData: booking.appointmentData,
+                email: booking.email,
+                treatment: booking.treatment
+            }
+
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.appointmentData}`
+                return res.send({ acknowledged: false, message })
+            }
+
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
