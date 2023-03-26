e6d9fa095c7ce8bb11486699389685956db58af1
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2023-03-26 16:13:25 +0600

    dashboard create,display appointment data and save the registered info related work

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ async function run() {
 
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
 
+        const usersCollection = client.db('doctorsPortal').collection('users');
+
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
             const query = {};
@@ -50,6 +52,15 @@ async function run() {
             res.send(options);
         });
 
+
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
@@ -67,7 +78,14 @@ async function run() {
 
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
         })
+
     }
     finally {
 
